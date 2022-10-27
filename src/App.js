18462649c59efec39eb62648b1412d264d8f2e8e
Archit Pattanaik <98776672+archit-2003@users.noreply.github.com>
2022-10-27 12:22:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import axios from 'axios';
 
 function App() {
-  let[value, setValue] = useState("");
+  let[sportData, setSportData] = useState("");
 
   const options = {
     method: 'GET',
@@ -18,32 +18,32 @@ function App() {
   
   axios.request(options).then(function (response) {
     console.log(response.data);
-    let data=response.data.map((a)=>{return (        <div className="p-10">  
+    let sportsNewsData=response.data.map((sport)=>{return (        <div className="p-10">  
 
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">{a.title}</div>
+      <div className="font-bold text-xl mb-2">{sport.title}</div>
       <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{a.group}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{sport.group}</span>
       </div>
         <p className="text-gra  y-700 text-base">
-          {a.description}
+          {sport.description}
         </p>
       </div>
     </div>
   </div>)})
-    console.log(data);
-    setValue(data);
+    console.log(sportsNewsData);
+    setSportData(sportsNewsData);
     
   }).catch(function (error) {
-    console.error(error);
+    console.error("Some error occured try again");
   });
 
   return (
     <div className="App">
       <Navbar />
       <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      {value ? value : <h1 className="font-bold text-center text-xl mb-2">Daily limit exceeded.</h1>}
+      {sportData ? sportData : <h1 className="font-bold text-center text-xl mb-2">Daily limit exceeded.</h1>}
       </div> 
 
  
